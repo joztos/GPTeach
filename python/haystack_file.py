240347fecb3575e6fe103c66e4af87e1aa9890e4
vito vekic <vitovekic913@gmail.com>
2023-03-01 19:20:41 +0100

    stuff/things

--- python/haystack_file.py
+++ python/haystack_file.py
@@ -1,65 +1,40 @@
 import logging
-import os
 from pathlib import Path
+import os 
 
-from haystack import Pipeline, Document
-from haystack.nodes import PDFToTextConverter, TextConverter
+from haystack.document_stores import ElasticsearchDocumentStore
+from haystack.nodes import PDFToTextConverter, EmbeddingRetriever
+from haystack.utils import launch_es
 
 logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.WARNING)
 logging.getLogger("haystack").setLevel(logging.WARNING)
 
-
-# Read PDF files from a local directory
-pdf_dir = Path("/pdfs")
-pdf_paths = list(pdf_dir.glob("*.pdf"))
-
-# Convert PDFs to text and create documents
 documents = []
-for pdf_path in pdf_paths:
-    converter = PDFToTextConverter(remove_numeric_tables=False, keep_physical_layout=False)
-    text = converter.convert(file_path=pdf_path)
-    document = Document(content=text, meta={"name": pdf_path.stem})
-    documents.append(document)
 
-print(f"Number of documents: {len(documents)}")
-
-# Initialize ElasticsearchDocumentStore
-launch_es()
 host = os.environ.get("ELASTICSEARCH_HOST", "localhost")
+
+converter = PDFToTextConverter(remove_numeric_tables=True, valid_languages=["en"])
+docs = converter.convert(file_path=Path("python\pdfs\9781785040207.pdf"), meta={"name": "test"})
+
+launch_es()
+
 document_store = ElasticsearchDocumentStore(
-    host=host,
+    host = host,
     username="",
     password="",
     index="document-small-test",
-    search_fields=["content", "meta.name"],
+    search_fields=["title","text"],
     embedding_field="embedding",
     excluded_meta_data=["embedding"],
     embedding_dim=768,
 )
 
-# Write documents to Elasticsearch
-document_store.write_documents(documents)
-
-# Initialize EmbeddingRetriever
 retriever = EmbeddingRetriever(
-    document_store=document_store,
-    embedding_model="deepset/sentence_bert",
-    model_format="farm",
-    use_gpu=False,
-)
-
-# Update document embeddings in Elasticsearch
-document_store.update_embeddings(retriever)
-
-# Initialize OpenAIAnswerGenerator
-generator = OpenAIAnswerGenerator(
-    model="text-davinci-002",
+    document_store=document_store, 
+    embedding_model="text-embedding-ada-002",
     api_key="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",
-)
+    max_seq_len=500,
+    )
 
-# Initialize GenerativeQAPipeline
-pipeline = GenerativeQAPipeline(generator=generator, retriever=retriever)
-
-# Run pipeline to get answers to a question
-result = pipeline.run(query="What is the capital city of France?", params={"Retriever": {"top_k": 5}})
-print(result)
+document_store.write_documents(docs)
+document_store.update_embeddings(retriever)
